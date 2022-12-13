255a19db4d805f1959ff745cc3cf074ce63a8e07
emmazaragoza <rubenemmanuelzaragoza@yahoo.com.ar>
2022-12-12 22:46:02 -0300

    arreglo port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import server from "./src/routes/index.js";
 const app = express()
 
 app.use('/', server)
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
     console.log(`servidor escuchando en el ${port}`)
